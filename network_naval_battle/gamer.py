ebb0037ee4088397c2b91f5cb4940d488aa484f7
Smet <dimkazhr@gmail.com>
2020-04-26 06:32:10 +0300

    create Gamer

--- network_naval_battle/gamer.py
+++ network_naval_battle/gamer.py
@@ -1,2 +1,17 @@
 class Gamer:
-    pass
+    def __init__(self, socket, field, name):
+        self.socket = socket
+        self.field = field
+        self.name = name
+
+    @property
+    def get_socket(self):
+        return self.socket
+
+    @property
+    def get_field(self):
+        return self.field
+
+    @property
+    def get_name(self):
+        return self.name
