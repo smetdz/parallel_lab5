7e57fe96dd9c95493dca6918dc092b6addef49cb
Smet <dimkazhr@gmail.com>
2020-05-04 15:40:02 +0300

    Some fixes.

--- network_naval_battle/ship.py
+++ network_naval_battle/ship.py
@@ -1,15 +1,15 @@
 class Ship:
     def __init__(self, x: int = None, y: int = None):
-        self.__is_alive = True
+        self._is_alive = True
         self._x = x
         self._y = y
 
     @property
     def is_alive(self):
-        return self.__is_alive
+        return self._is_alive
 
     def kill(self):
-        self.__is_alive = False
+        self._is_alive = False
 
     def __str__(self):
         if self.is_alive:
